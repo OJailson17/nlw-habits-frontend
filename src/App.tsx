94bd3640e55da5c43d4674d08163331ae6048aec
OJailson17 <JaylsonO17@gmail.com>
2023-01-18 21:17:06 -0300

    chore: finish layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Habit } from './components/Habit';
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
 export function App() {
 	return (
-		<>
-			<Habit completed={3} />
-			<Habit completed={5} />
-			<Habit completed={1} />
-			<Habit completed={2} />
-		</>
+		<div className='w-screen h-screen flex justify-center items-center'>
+			<div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+				<Header />
+				<SummaryTable />
+			</div>
+		</div>
 	);
 }
